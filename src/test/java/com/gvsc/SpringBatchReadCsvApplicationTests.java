ac71a951109edc5cfd5b427d2042a862d0c308b0
Gabriel <gvscceca@gmail.com>
2021-12-01 14:34:20 -0300

    finish the tests

---
+++ src/test/java/com/gvsc/SpringBatchReadCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchReadCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
